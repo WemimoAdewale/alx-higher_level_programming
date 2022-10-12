dc1fcf77229eafda3c562b41106b255dac4bb66a
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-10-11 22:01:37 -0700

    task 6 ipdated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,7 +3,7 @@
 
 
 class Square:
-    """Tepresent a squate"""
+    """Represent a square"""
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
@@ -52,8 +52,8 @@ class Square:
             print("")
             return
 
-    [print("") for i in range(0, self.__position[1])]
-    for i in range(0, self.__size):
-        [print(" ", end="") for j in range(0, self.__position[0])]
-        [print("#", end="") for k in range(0, self.__size)]
-        print("")
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
