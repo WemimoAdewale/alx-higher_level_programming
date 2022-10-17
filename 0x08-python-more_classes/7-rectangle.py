141d9e0c12ede26efdabc6e2c7ea064d65fc95a0
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-10-17 11:59:44 -0700

    7 appeded

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -10,6 +10,9 @@ class Rectangle:
         print_symbol (any): The symbol used for string representation.
         """
 
+    number_of_instances = 0
+    print_symbol = "#"
+    
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
@@ -17,6 +20,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
