2b1966c12370082a6efe7f1a2b52d53786baa402
WemimoAdewale <adewaleadewemimoesther@gmail.com>
2022-10-25 20:12:36 -0700

    task10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -17,7 +17,8 @@ class Student:
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
 
-        If attrs is a list of strings, represents only those attributes invluded in string
+        If attrs is a list of strings, represents only those attributes
+        invluded in string
 
         Args:
             attrs (list): (Optional) The attributes to represent.
